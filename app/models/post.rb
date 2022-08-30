c9d16b1d14d1e221f64fbd3b36d2976fdac2ef65
Lucas Duncan <lucasdonuts@gmail.com>
2022-08-30 09:00:00 -0500

    All tests passing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :title_is_clickbait
+
+  def title_is_clickbait
+    bait_phrases = ["Won't Believe", "Secret", "Top ", "Guess"]
+
+    unless !title || bait_phrases.any? { |phrase| title.include? phrase }
+      errors.add( :title, "Title not clickbait-y enough.")
+    end
+
+  end
 end
